4b0d4ca9857d8d187ce45ef66a1c577ea1dd082a
Anwesh Mishra <anweshm3@gmail.com>
2022-12-12 02:18:01 +0530

    Updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
   const firebaseAuth = getAuth(app)
   const provider = new GoogleAuthProvider()
 
-  const [{ user }, dispatch] = useStateValue()
+  const [{ user, cartShow, cartItems }, dispatch] = useStateValue()
 
   const [isMenu, setIsMenu] = useState(false);
 
@@ -47,6 +47,13 @@ function Header() {
     })
   }
 
+  const showCart = () => {
+    dispatch({
+      type: actionType.SET_CART_SHOW,
+      cartShow: !cartShow,
+    })
+  }
+
   return (
     <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
       {/* desktop & tablet */}
@@ -66,13 +73,15 @@ function Header() {
             <li className='text-xl text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
           </motion.ul>
           {/* Cart */}
-          <div className='relative flex items-center justify-center'>
+          <div className='relative flex items-center justify-center' onClick={showCart}>
             <MdShoppingCart className='text-textColor text-2xl cursor-pointer' />
-            <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            {cartItems && cartItems.length > 0 && (
+              <div className="absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-bold">
-                2
+                {cartItems.length}
               </p>
             </div>
+            )}
           </div>
           <div className='relative'>
             {/* Profile Image */}
@@ -101,13 +110,15 @@ function Header() {
         </Link>
         <div className='relative flex items-center gap-8'>
           {/* Cart */}
-          <div className='relative flex items-center justify-center'>
+          <div className='relative flex items-center justify-center' onClick={showCart}>
             <MdShoppingCart className='text-textColor text-xl cursor-pointer' />
-            <div className="absolute -top-2 -right-2 w-3 h-3 rounded-full bg-cartNumBg flex items-center justify-center">
+            {cartItems && cartItems.length > 0 && (
+              <div className="absolute -top-2 -right-2 w-3 h-3 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-[10px] text-white font-medium">
-                2
+                {cartItems.length}
               </p>
             </div>
+            )}
           </div>
           {/* Profile Image */}
           <motion.img whileTap={{ scale: 0.6 }} src={user ? user.photoURL : Avatar} alt="profile logo" className='w-10 h-10 min-w-[40px] min-h-[40px] drop-shadow-2xl cursor-pointer rounded-full' onClick={login} referrerPolicy="no-referrer" />
