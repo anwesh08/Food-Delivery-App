4239377e06f842d7647a0860a9ebd200d7a13d37
Anwesh Mishra <anweshm3@gmail.com>
2022-12-12 02:18:09 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <AnimatePresence mode="wait">
-      <div className="h-auto w-screen flex flex-col bg-primary">
+      <div className="h-full w-screen flex flex-col bg-primary">
         <Header />
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
